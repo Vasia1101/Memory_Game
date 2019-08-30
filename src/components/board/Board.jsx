155e948c0b243d431ad0578cebe457819cfcd67b
Vasia1101 <vasilij.gajda@gmail.com>
2019-08-30 15:19:24 +0300

    fix_import

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Card from '../Card/Card';
+import Card from '../card/Card';
 
 function Board({ cards, step, handleClick, idx, overturn }) {
   return (
